a7899d2a4da552cbf90a69298af5c501cbec9b51
sslivkoff <stormslivkoff@gmail.com>
2022-05-08 11:04:50 -0700

    bump to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='toolplot',
-    version='0.0.1',
+    version='0.1.0',
     packages=['toolplot'],
     install_requires=[
         'matplotlib',
--- toolplot/__init__.py
+++ toolplot/__init__.py
@@ -3,3 +3,6 @@ from .plot_save import *
 from .plot_setup import *
 from .plot_ticks import *
 from .special_plots import *
+
+
+__version__ = '0.1.0'
