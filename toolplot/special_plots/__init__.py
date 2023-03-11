adea8e2572cfc78c2399209ce0575497627e4c86
sslivkoff <stormslivkoff@gmail.com>
2023-03-10 18:35:07 -0800

    add hist_log_x()

--- toolplot/special_plots/__init__.py
+++ toolplot/special_plots/__init__.py
@@ -1,2 +1,3 @@
 from .bar_plots import *
 from .candlestick_plots import *
+from .log_histograms import *
---
+++ toolplot/special_plots/log_histograms.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def hist_log_x(x, *bin_args, xlim=None, n_bins=30, bin_min=None, **bin_kwargs):
+    if xlim is None:
+        if bin_min is None:
+            xmin = max(min(x), 0.00000000001)
+        else:
+            xmin = bin_min
+        xlim = [xmin, max(x)]
+    bins = np.logspace(np.log10(xlim[0]), np.log10(xlim[1]), n_bins)
+
+    plt.hist(x, *bin_args, bins=bins, **bin_kwargs)
+
+    #     counts, bins = np.histogram(x, bins)
+    #     plt.hist(counts, bins=bins, **bin_kwargs)
+
+    plt.xscale('log')
+
